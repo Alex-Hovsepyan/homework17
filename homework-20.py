edb98498db537f4bd11e3b014061ce9868749f76
Alex15Hovsepyan <alextyoou@gmail.com>
2023-02-22 09:39:44 +0400

    Alex

---
+++ homework-20.py
@@ -0,0 +1,10 @@
+def func():
+    summ = 0
+    with open('numbers.txt', 'r') as file:
+        li = file.read().replace('\n', ' ').split(' ')
+        for i in li:
+            if i.isdigit():
+                summ += int(float(i))
+    with open('answer.txt', 'a') as file2:
+        file2.write('gf')
+func()
